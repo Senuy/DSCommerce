cd2790f0d1dfcfb7a3c06a9bcf6a2787a65d4d62
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-06-17 09:17:04 -0400

    User Modification

--- src/main/java/com/devyunes/dscommerce/entities/User.java
+++ src/main/java/com/devyunes/dscommerce/entities/User.java
@@ -3,6 +3,7 @@ package com.devyunes.dscommerce.entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
